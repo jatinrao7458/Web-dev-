4e989a8b04b1dbfa77b96f7620711bc0dd89ed41
jatinrao7458 <jatinrao7458@gmail.com>
2025-10-13 20:52:58 +0530

    adding ternary operater info

---
+++ javascript/video practice/ternary_operater.js
@@ -0,0 +1,6 @@
+// similiar to if else statement but a ingle line if else like
+a = 3;
+b=4;
+c= b>a ?(b-a):(a-b);
+console.log(c)
+// this code means that c is a variable where if b>a then do b-a or if b>a the do a-b

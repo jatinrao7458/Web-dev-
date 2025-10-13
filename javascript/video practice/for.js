2b0c400a76f28fce2ad9b42b4cdd8367d2e2dc2f
jatinrao7458 <jatinrao7458@gmail.com>
2025-10-13 21:10:39 +0530

    adding for loop

---
+++ javascript/video practice/for.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+    
+}
+// goes like  that before first ; it means you are defining a variable after that 
+// 2nd step is condition which means that till this value the loop will only work
+//  3 step is increment or decrement means that after once the loop runs then what to do 
+// {in this the code is written which is to exceuted}

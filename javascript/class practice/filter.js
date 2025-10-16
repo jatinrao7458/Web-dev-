da4ca56de4e69cffcf8e7feb49242a13f29ace54
jatinrao7458 <jatinrao7458@gmail.com>
2025-10-16 21:00:10 +0530

    adding reduce and map function in js

---
+++ javascript/class practice/filter.js
@@ -0,0 +1,5 @@
+let u = [1,4,2,4,5,7,9]
+let y = u.filter((ii)=>{
+    return ii%2==1
+})
+console.log(y)
---
+++ javascript/class practice/map.js
@@ -0,0 +1,5 @@
+let y = [1,2,3,4,5]
+let i = y.map((i) =>{
+    return  i**2
+})
+console.log(i)

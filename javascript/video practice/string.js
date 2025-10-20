6e062996652165454699d3053a57c9221e737ccd
jatinrao7458 <jatinrao7458@gmail.com>
2025-10-20 09:45:30 +0530

    adding understanding content

--- javascript/video practice/string.js
+++ javascript/video practice/string.js
@@ -12,11 +12,19 @@ console.log(a.length)
 let name= "harry"
 let frnd ="jatin"
 console.log(" his name is " + name + " and his frnd name is " + frnd)
+// line 16 is an example of template lateral we insert it by using backtick ` (below esc) 
 console.log(`his name is ${name} and his frnds name is ${frnd}`)
+// examples of escape sequence
+// \n -, addes new line
+
 let b= "shivam"
 console.log(b.toUpperCase())
 console.log(b.toLowerCase())
+// slice - slice the element from 1 to 4 by not including 4
+// there is a similiar function called splice which is use for array and in that if we say splice(1,4) then it will splice form 1 to 4 index number with including 4 with it  .
 console.log(b.slice(1, 4))
+// replace remove sh to 88, if there are 2 repetion then only first one will be changed
 console.log(b.replace("sh", "88"))
 console.log(b.concat(a, " ayush "))
+// string are immutable i.e the intial string will be same even after replace ,concat etc.
 console.log(b)
